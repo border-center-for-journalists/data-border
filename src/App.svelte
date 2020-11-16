d62d8dc493e56b54ff0cc6ba20909273edcbf54b
El Sonny <sonny@spaceshiplabs.com>
2020-11-16 17:54:13 +0100

    migrate to svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<main>
+	<nav class='main-nav'>
+        <button class='menu-button'></button>
+        <a href='/' class='logo'> <img alt='data border logo' src='img/logo.png'> </a>
+        <nav class='social'>
+            <a href='https://www.facebook.com/bordercenter' class='fb'> </a>
+            <a href='https://twitter.com/BorderCenter' class='tw'> </a>
+            <a href='https://www.youtube.com/channel/UCZY_GRVRu8Gxz_DrRO6hgsA' class='yt'> </a>
+        </nav>
+    </nav>
+    <section class='projects'>
+        <article>
+            <header>
+                <nav>
+                    <img src='img/app-icon.png' alt='app-icon' />
+                    <span>APP</span>
+                </nav>
+                <h2>Facturanet</h2>
+            </header>
+            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <div class='bg-img'><img src='img/projects/facturanet.png' alt='Captura de pantalla de pagina de border hub' /></div>
+            <div class='bg-color'></div>
+        </article>
+    </section>
+</main>
+
+<style>
+
+</style>
